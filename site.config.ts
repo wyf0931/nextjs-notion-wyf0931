7f9cbc96dbf155d8ce55890e39d71d4213113659
wyf0931 <wyf0931@gmail.com>
2023-03-16 01:05:55 +0800

    update domain

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Wyf0931技术博客',
-  domain: 'notion-blog-wyf0931.vercel.app',
+  domain: 'nextjs-notion-wyf0931.vercel.app',
   author: 'wyf0931',
 
   // open graph metadata (optional)
